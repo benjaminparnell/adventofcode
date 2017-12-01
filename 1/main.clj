5764f0312d6e0ca410b9cc8f31999528ba3ddb35
Benjamin Parnell <benjaminparnell.94@gmail.com>
2017-12-01 13:03:33 +0000

    Complete stage two of the first challenge

--- 1/main.clj
+++ 1/main.clj
@@ -3,3 +3,12 @@
 (defn captcha [string]
   (let [ numbers (map #(Integer/parseInt %) (str/split (str string (first string)) #"")) ]
     (reduce + (keep-indexed #(if (== (nth numbers (- %1 1) 0) %2) %2) numbers))))
+
+(defn halfway [coll index]
+  (if (>= (+ index (/ (count coll) 2)) (count coll))
+    (nth coll (- (+ index (/ (count coll) 2)) (count coll)))
+    (nth coll (+ index (/ (count coll) 2)))))
+
+(defn captchaTwo [string]
+  (let [ numbers (map #(Integer/parseInt %) (str/split string #"")) ]
+    (reduce + (keep-indexed #(if (== (halfway numbers %1) %2) %2) numbers))))
