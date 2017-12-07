861d05fa782d56499747fea349d4d79f98c3a2ac
Benjamin Parnell <benjaminparnell.94@gmail.com>
2017-12-07 15:36:05 +0000

    Add day 7 part 1

---
+++ 7/main.clj
@@ -0,0 +1,21 @@
+(require '[clojure.string :as str])
+
+(defn parse-program-line [line]
+  (hash-map :name (first (str/split line #" ")),
+            :weight (Integer. (re-find #"\d+" (get (str/split line #" ") 1))),
+            :children (str/split (or (get (str/split line #"-> ") 1) "") #", ")))
+
+(defn parent-of [node tree]
+  (first (filter #(some #{(node :name)} (%1 :children)) tree)))
+
+(defn find-root [tree]
+  (loop [node (first tree)]
+    (if (not (parent-of node tree))
+      node
+      (recur (parent-of node tree)))))
+
+(defn get-node [tree node]
+  (first (filter #(= (%1 :name) (node :name)) tree)))
+
+(defn make-program-tree [path]
+  (map parse-program-line ((comp str/split-lines slurp) path)))
