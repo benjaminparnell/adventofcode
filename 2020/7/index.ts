58401578cc2b251c7287a3a141cc98a59285a184
Benjamin Parnell <benjaminparnell.94@gmail.com>
2020-12-07 17:52:41 +0000

    Add part 2 of day 7 2020

--- 2020/7/index.ts
+++ 2020/7/index.ts
@@ -5,30 +5,35 @@ const lines = fs
   .readFileSync(path.join(__dirname, "input.txt"), "utf-8")
   .split("\n");
 
-const graph: Record<string, string[]> = lines.reduce((memo, line) => {
+interface BagNode {
+  name: string;
+  quantity: number;
+}
+type BagGraph = Record<string, BagNode[]>;
+
+const graph = lines.reduce<BagGraph>((memo, line) => {
   const [head, tail] = line.split(" bags contain ");
   const parts = tail.match(/[0-9]+ [\w\s]+ bags?/g);
   memo[head] = parts?.map((part) => {
     const matches = part.match(/([0-9]+) ([\w\s]+) bags?/);
-    return matches[2];
+    return {
+      quantity: parseInt(matches[1], 10),
+      name: matches[2],
+    };
   });
   return memo;
 }, {});
 
-const find = (
-  graph: Record<string, string[]>,
-  key: string,
-  needle: string
-): boolean => {
-  if (!graph[key]) {
+const find = (graph: BagGraph, start: string, target: string): boolean => {
+  if (!graph[start]) {
     return false;
   }
 
-  if (graph[key]?.includes(needle)) {
+  if (graph[start]?.some((bagNode) => bagNode.name === target)) {
     return true;
   }
 
-  return graph[key].some((child) => find(graph, child, needle));
+  return graph[start].some((child) => find(graph, child.name, target));
 };
 
 const bagsThatHoldGoldBags = Object.keys(graph).reduce(
@@ -38,3 +43,19 @@ const bagsThatHoldGoldBags = Object.keys(graph).reduce(
 );
 
 console.log("Part one:", bagsThatHoldGoldBags);
+
+const getBagCounts = (graph: BagGraph, key: string): number => {
+  if (!graph[key]) {
+    return 0;
+  }
+
+  return graph[key].reduce(
+    (count, child) =>
+      count + child.quantity * (getBagCounts(graph, child.name) + 1),
+    0
+  );
+};
+
+const bagsInGoldBag = getBagCounts(graph, "shiny gold");
+
+console.log("Part two:", bagsInGoldBag);
