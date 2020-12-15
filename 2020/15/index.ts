7e2495608643b31c18e9383fb49e4c318474033b
Benjamin Parnell <benjaminparnell.94@gmail.com>
2020-12-15 23:24:02 +0000

    Add part 2 day 15 2020

--- 2020/15/index.ts
+++ 2020/15/index.ts
@@ -1,33 +1,45 @@
 const input = [14, 1, 17, 0, 3, 20];
 
-const map = new Map<number, number[]>();
+const addInIndex = (
+  map: Map<number, number[]>,
+  value: number,
+  turnNumber: number
+) => {
+  const turns = map.get(value);
 
-input.forEach((number, index) => {
-  map.set(number, [index + 1]);
-});
-
-let turnNumber = input.length + 1;
-let lastNumber = input[input.length - 1];
-
-while (turnNumber <= 30000000) {
-  const turnsSpoken = map.get(lastNumber);
-  const spokenCount = turnsSpoken?.length;
-  if (spokenCount > 1) {
-    const newNumber = turnNumber - turnsSpoken[turnsSpoken.length - 2] - 1;
-    map.set(
-      newNumber,
-      map.has(newNumber)
-        ? map.get(newNumber).concat([turnNumber])
-        : [turnNumber]
-    );
-    lastNumber = newNumber;
-  } else {
-    map.set(0, map.has(0) ? map.get(0).concat([turnNumber]) : [turnNumber]);
-    lastNumber = 0;
+  if (turns) {
+    return [turns[turns.length - 1], turnNumber];
   }
-  turnNumber++;
-}
 
-console.log(map);
+  return [turnNumber];
+};
 
-console.log("Part one:", lastNumber);
+const playTheGameToTurn = (lastTurnNumber: number) => {
+  const map = new Map<number, number[]>();
+
+  input.forEach((number, index) => {
+    map.set(number, [index + 1]);
+  });
+
+  let turnNumber = input.length + 1;
+  let lastNumber = input[input.length - 1];
+
+  while (turnNumber <= lastTurnNumber) {
+    const turnsSpoken = map.get(lastNumber);
+    const spokenCount = turnsSpoken?.length;
+    if (spokenCount > 1) {
+      const newNumber = turnNumber - turnsSpoken[turnsSpoken.length - 2] - 1;
+      map.set(newNumber, addInIndex(map, newNumber, turnNumber));
+      lastNumber = newNumber;
+    } else {
+      map.set(0, addInIndex(map, 0, turnNumber));
+      lastNumber = 0;
+    }
+    turnNumber++;
+  }
+
+  return lastNumber;
+};
+
+console.log("Part one:", playTheGameToTurn(2020));
+console.log("Part two:", playTheGameToTurn(30000000));
