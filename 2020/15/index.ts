ea1ab63fdbb78665080d6ac8331d4613b08ec69e
Benjamin Parnell <benjaminparnell.94@gmail.com>
2020-12-15 23:02:40 +0000

    checkin

--- 2020/15/index.ts
+++ 2020/15/index.ts
@@ -1,21 +1,33 @@
 const input = [14, 1, 17, 0, 3, 20];
-const numbersSpoken = [...input];
 
-const count = (array: number[], target: number): number =>
-  array.reduce((memo, a) => memo + (a === target ? 1 : 0), 0);
+const map = new Map<number, number[]>();
 
-while (numbersSpoken.length !== 2020) {
-  const lastNumber = numbersSpoken[numbersSpoken.length - 1];
-  const spokenCount = count(numbersSpoken, lastNumber);
+input.forEach((number, index) => {
+  map.set(number, [index + 1]);
+});
 
+let turnNumber = input.length + 1;
+let lastNumber = input[input.length - 1];
+
+while (turnNumber <= 30000000) {
+  const turnsSpoken = map.get(lastNumber);
+  const spokenCount = turnsSpoken?.length;
   if (spokenCount > 1) {
-    const index =
-      numbersSpoken.slice(0, numbersSpoken.length - 1).lastIndexOf(lastNumber) +
-      1;
-    numbersSpoken.push(numbersSpoken.length - index);
+    const newNumber = turnNumber - turnsSpoken[turnsSpoken.length - 2] - 1;
+    map.set(
+      newNumber,
+      map.has(newNumber)
+        ? map.get(newNumber).concat([turnNumber])
+        : [turnNumber]
+    );
+    lastNumber = newNumber;
   } else {
-    numbersSpoken.push(0);
+    map.set(0, map.has(0) ? map.get(0).concat([turnNumber]) : [turnNumber]);
+    lastNumber = 0;
   }
+  turnNumber++;
 }
 
-console.log("Part one:", numbersSpoken[numbersSpoken.length - 1]);
+console.log(map);
+
+console.log("Part one:", lastNumber);
