6e1608e434a76d7cef0aa900d77209664d86c3fb
Ben Parnell <benjaminparnell.94@gmail.com>
2021-12-05 15:21:42 +0000

    Add part 2 of the day 3 2021

--- 2021/3/index.ts
+++ 2021/3/index.ts
@@ -3,7 +3,8 @@ import path from "path";
 
 const lines = fs
   .readFileSync(path.join(__dirname, "input.txt"), "utf-8")
-  .split("\n");
+  .split("\n")
+  .filter(Boolean);
 
 const getMostCommon = (lines: string[]) => {
   let counts = [];
@@ -35,3 +36,72 @@ const flip = (input: string) => {
 const mostCommon = getMostCommon(lines);
 const leastCommon = flip(mostCommon);
 console.log(parseInt(mostCommon, 2) * parseInt(leastCommon, 2));
+
+const getMostCommonInPosition = (lines: string[], position: number) => {
+  let zero = 0;
+  let one = 0;
+  for (let line of lines) {
+    if (line[position] === "0") {
+      zero++;
+    } else {
+      one++;
+    }
+  }
+
+  if (zero > one) {
+    return "0";
+  }
+  return "1";
+};
+
+const flipPos = (input: string) => input === '1' ? '0' : '1'
+
+const getOxygenRating = (lines: string[]) => {
+  let found = false;
+  let currentPosition = 0;
+
+  while (!found) {
+    if (lines.length === 1) {
+      found = true;
+    } else {
+      const mostCommonInPosition = getMostCommonInPosition(
+        lines,
+        currentPosition
+      );
+
+      lines = lines.filter(
+        (line) => line[currentPosition] === mostCommonInPosition
+      );
+
+      currentPosition++;
+    }
+  }
+
+  return lines[0];
+};
+
+const getCo2Rating = (lines: string[]) => {
+  let found = false;
+  let currentPosition = 0;
+
+  while (!found) {
+    if (lines.length === 1) {
+      found = true;
+    } else {
+      const leastCommonInPosition = flipPos(getMostCommonInPosition(
+        lines,
+        currentPosition
+      ));
+
+      lines = lines.filter(
+        (line) => line[currentPosition] === leastCommonInPosition
+      );
+
+      currentPosition++;
+    }
+  }
+
+  return lines[0];
+};
+
+console.log(parseInt(getOxygenRating(lines), 2) * parseInt(getCo2Rating(lines), 2));
