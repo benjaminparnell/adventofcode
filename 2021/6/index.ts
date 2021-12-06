07184b63d738b579d569431894ceae1703e2baf8
Ben Parnell <benjaminparnell.94@gmail.com>
2021-12-06 14:34:40 +0000

    Add part 2 day 6 2021

--- 2021/6/index.ts
+++ 2021/6/index.ts
@@ -6,20 +6,27 @@ const lines = fs
   .split("\n")
   .filter(Boolean);
 
-let fish = lines[0].split(',').map(item => parseInt(item, 10))
+let fish = lines[0].split(',').map(item => ({ count: 1, days: parseInt(item, 10) }))
 
 for (let day = 0; day < 256; day++) {
     const startingFishLength = fish.length
+    let newFish = 0;
 
     for (let fishIndex = 0; fishIndex < startingFishLength; fishIndex++) {
         const singleFish = fish[fishIndex]
-        if (singleFish === 0) {
-            fish.push(8)
-            fish[fishIndex] = 6
+        if (singleFish.days === 0) {
+            fish[fishIndex].days = 6
+            newFish += fish[fishIndex].count
         } else {
-            fish[fishIndex] = singleFish - 1
+            fish[fishIndex].days = singleFish.days - 1
         }
     }
+
+    fish.push({ count: newFish, days: 8 })
 }
 
-console.log(fish.length)
+const numberOfFish = fish.reduce((memo, item) => {
+    return memo + item.count
+}, 0)
+
+console.log(numberOfFish)
